cf89eec3a5e3fcd06b15869456d6b27a6349e603
SharfrazAhmad <shkrfarazahmad456@gmail.com>
2024-06-10 21:40:22 +0530

    Your commit message here

---
+++ CheckNumber Model/script.js
@@ -0,0 +1,52 @@
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("openModalBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+const paracount = document.getElementById('count');
+let c = paracount.textContent;
+function incr ()
+{
+    if(c>19){
+        alert("Number can't be more than 20");
+        
+    }
+    else{
+        c++;
+    paracount.textContent=c
+    }
+}
+function decr ()
+{
+    if(c<=0){
+        alert("Number can't be less than 0");
+        
+    }
+    else{
+        c--;
+        paracount.textContent = c;
+    }
+}
+function reset ()
+{
+    c = 0;
+    document.getElementById("result").innerText="0";
+    paracount.textContent=c
+}
+function checkNumber(){
+    if(c==0){
+        document.getElementById("result").innerText="0";
+    }
+    else if(c%2==0){
+        document.getElementById("result").innerText="Even";
+    }
+    else{
+        document.getElementById("result").innerText="Odd";
+    }
+}
